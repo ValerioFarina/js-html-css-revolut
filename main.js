97a26503ac3f251daebbdeed1b0dd54faa9f8f57
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-28 15:22:39 +0100

    Intercettato il mouseenter delle voci del menu nel nav-right

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-    
+    // intercetto il mouseenter delle voci del menu nel nav-right
+    $('#nav-right .menu > li').mouseenter(function() {
+        alert('sei sopra');
+    });
 
 // fine document.ready
 });
