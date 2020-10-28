22263f10e07e8e9cb178861e175331e19fa8551b
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-28 15:32:49 +0100

    Fatto in modo che quando si va con il mouse sopra una delle voci del menu compare il corrispondente dropdown-menu

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
     // intercetto il mouseenter delle voci del menu nel nav-right
     $('#nav-right .menu > li').mouseenter(function() {
-        alert('sei sopra');
+        // seleziono il dropdown-menu che è figlio della voce corrente (cioè, della voce sulla quale sono andato con il mouse)
+        // tolgo a questo dropdown-menu la classe hidden
+        $(this).children('.dropdown-menu').removeClass('hidden');
     });
 
 // fine document.ready
